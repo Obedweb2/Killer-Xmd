73d6a036a5e5eb31c54f27b178897e200e9247bf
Obedweb2 <rongouniversity77@gmail.com>
2025-05-22 03:40:14 +0300

    Update dl-fb.js

--- plugins/dl-fb.js
+++ plugins/dl-fb.js
@@ -31,7 +31,7 @@ cmd({
     const videoUrl = data.data.url;
     await conn.sendMessage(from, {
       video: { url: videoUrl },
-      caption: "📥 *Facebook Video Downloaded*\n\n- Powered By JawadTechX ✅",
+      caption: "📥 *Facebook Video Downloaded*\n\n- Powered By 🄾🄱🄴🄳 🅃🄴🄲🄷 ✅",
     }, { quoted: m });
 
   } catch (error) {
