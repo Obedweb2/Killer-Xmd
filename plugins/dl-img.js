2d92a34ec9074cf2dfa2eb749e634edd0af443ba
Obedweb2 <rongouniversity77@gmail.com>
2025-05-22 03:41:09 +0300

    Update dl-img.js

--- plugins/dl-img.js
+++ plugins/dl-img.js
@@ -37,7 +37,7 @@ cmd({
                 from,
                 { 
                     image: { url: imageUrl },
-                    caption: `📷 Result for: ${query}\n> © Powered by JawadTechX`
+                    caption: `📷 Result for: ${query}\n> © Powered by 🄾🄱🄴🄳 🅃🄴🄲🄷
                 },
                 { quoted: mek }
             );
